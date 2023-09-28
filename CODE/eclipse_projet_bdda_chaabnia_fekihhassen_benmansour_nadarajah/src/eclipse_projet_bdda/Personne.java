12eec4eb1ab688afaf82d71af6bd06d0d159c54a
akramch <akram.chaabnia25@gmail.com>
2023-09-28 10:40:57 +0200

    premier tp

---
+++ CODE/eclipse_projet_bdda_chaabnia_fekihhassen_benmansour_nadarajah/src/eclipse_projet_bdda/Personne.java
@@ -0,0 +1,15 @@
+package eclipse_projet_bdda;
+
+public class Personne {
+	private String name;
+	
+	public Personne(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	
+}
