04dbb75a304bfa8932f61e4de9894009fdd19a9a
snaazy <yassinefekih78@outlook.fr>
2023-10-11 22:44:30 +0200

    DiskManagerTests class : Tests

--- CODE/eclipse_projet_bdda_chaabnia_fekihhassen_benmansour_nadarajah/src/eclipse_projet_bdda/DiskManagerTests.java
+++ CODE/eclipse_projet_bdda_chaabnia_fekihhassen_benmansour_nadarajah/src/eclipse_projet_bdda/DiskManagerTests.java
@@ -1,5 +1,7 @@
 package eclipse_projet_bdda;
 
+import java.io.IOException;
+import java.nio.ByteBuffer;
 
 public class DiskManagerTests {
 
@@ -7,4 +9,66 @@ public class DiskManagerTests {
 		// TODO Auto-generated method stub
 
 	}
+
+	// test allocation de page
+	public static void testAllocPage() {
+        DiskManager diskManager = DiskManager.getInstance();
+        PageId pageId1 = diskManager.AllocPage();
+        PageId pageId2 = diskManager.AllocPage();
+        
+		// on vérifie que les PageId sont différents
+        assert pageId1 != null;
+        assert pageId2 != null;
+        assert pageId1.getPageIdx() != pageId2.getPageIdx();
+        
+        System.out.println("testAllocPage passé");
+    }
+
+	 // test de lecture de page
+	 public static void testReadPage() {
+        DiskManager diskManager = DiskManager.getInstance();
+        PageId pageId = diskManager.AllocPage();
+        ByteBuffer buffer = ByteBuffer.allocate(DBParams.SGBDPageSize);
+        byte[] data = "coucou".getBytes();
+        buffer.put(data);
+        buffer.flip();
+
+        try {
+            diskManager.WritePage(pageId, buffer);
+            buffer.clear();
+            diskManager.ReadPage(pageId, buffer);
+            buffer.flip();
+            byte[] result = new byte[buffer.remaining()];
+            buffer.get(result);
+            assert new String(result).equals("coucou");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+        System.out.println("testReadPage passé");
+    }
+
+	  // test d'écriture de page
+	  public static void testWritePage() {
+        DiskManager diskManager = DiskManager.getInstance();
+        PageId pageId = diskManager.AllocPage();
+        ByteBuffer buffer = ByteBuffer.allocate(DBParams.SGBDPageSize);
+        byte[] data = "coucou".getBytes();
+        buffer.put(data);
+        buffer.flip();
+
+        try {
+            diskManager.WritePage(pageId, buffer);
+            buffer.clear();
+            diskManager.ReadPage(pageId, buffer);
+            buffer.flip();
+            byte[] result = new byte[buffer.remaining()];
+            buffer.get(result);
+            assert new String(result).equals("coucou");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+        System.out.println("testWritePage passé");
+    }
 }
