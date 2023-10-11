34961924bf3b0c8f3c55fcba824fbdc288f05985
snaazy <yassinefekih78@outlook.fr>
2023-10-11 22:18:16 +0200

    DBParams class : JavaDoc

--- CODE/eclipse_projet_bdda_chaabnia_fekihhassen_benmansour_nadarajah/src/eclipse_projet_bdda/DBParams.java
+++ CODE/eclipse_projet_bdda_chaabnia_fekihhassen_benmansour_nadarajah/src/eclipse_projet_bdda/DBParams.java
@@ -1,11 +1,15 @@
 package eclipse_projet_bdda;
-
-
+/**
+ * Classe contenant les paramètres de la base de données
+ */
 public class DBParams {
+	// chemin vers le dossier de la bdd
 	public static String DBPath;
+	// taille d'une page 
 	public static int SGBDPageSize;
+	// nombre max de fichiers gérés par le DM
 	public static int DMFileCount;
-	
+	// compteur de trames
 	public static int frameCount;
 	
 }
