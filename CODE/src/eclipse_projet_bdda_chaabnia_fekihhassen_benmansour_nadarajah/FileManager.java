b673b88dad7ad3f43f23c5134234fcac8dca5040
snaazy <yassinefekih78@outlook.fr>
2023-12-06 21:30:07 +0100

    adddatapage

--- CODE/src/eclipse_projet_bdda_chaabnia_fekihhassen_benmansour_nadarajah/FileManager.java
+++ CODE/src/eclipse_projet_bdda_chaabnia_fekihhassen_benmansour_nadarajah/FileManager.java
@@ -40,6 +40,46 @@ public class FileManager {
         return newHeaderPageId;
     }
     
+    public PageId addDataPage(TableInfo tabInfo) throws IOException, PageNotFoundException {
+        DiskManager dm = DiskManager.getInstance();
+        PageId newDataPageId = dm.allocPage();
+    
+        BufferManager bm = BufferManager.getInstance();
+        ByteBuffer newDataPageBuffer = bm.getPage(newDataPageId);
+    
+        // initialise la nouvelle page de donnees
+        for (int i = 0; i < DBParams.SGBDPageSize / 8; i++) {
+            newDataPageBuffer.putInt(4 + i * 8, 0); // initialise les slots
+            newDataPageBuffer.putInt(8 + i * 8, 0); // initialise les tailles
+        }
+        bm.freePage(newDataPageId, true);
+    
+        PageId headerPageId = tabInfo.getHeaderPageId();
+        ByteBuffer headerPageBuffer = bm.getPage(headerPageId);
+    
+        int firstFreePageFileIdx = headerPageBuffer.getInt(0);
+        int firstFreePagePageIdx = headerPageBuffer.getInt(4);
+    
+        if (firstFreePageFileIdx == -1 && firstFreePagePageIdx == 0) {
+            headerPageBuffer.putInt(0, newDataPageId.getFileIdx());
+            headerPageBuffer.putInt(4, newDataPageId.getPageIdx());
+        } else {
+            PageId lastPageId = new PageId(firstFreePageFileIdx, firstFreePagePageIdx);
+            ByteBuffer lastPageBuffer = bm.getPage(lastPageId);
+            while (lastPageBuffer.getInt(0) != -1) {
+                lastPageId = new PageId(lastPageBuffer.getInt(0), lastPageBuffer.getInt(4));
+                lastPageBuffer = bm.getPage(lastPageId);
+            }
+    
+            lastPageBuffer.putInt(0, newDataPageId.getFileIdx());
+            lastPageBuffer.putInt(4, newDataPageId.getPageIdx());
+            bm.freePage(lastPageId, true);
+        }
+    
+        bm.freePage(headerPageId, true);
+        return newDataPageId;
+    }
+    
     
     
 }
