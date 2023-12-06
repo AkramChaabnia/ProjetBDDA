88ba9dc90433a0c55d47c67ac8b2b7e4629f16ab
snaazy <yassinefekih78@outlook.fr>
2023-12-06 19:12:35 +0100

    rectification

--- CODE/src/eclipse_projet_bdda_chaabnia_fekihhassen_benmansour_nadarajah/Record.java
+++ CODE/src/eclipse_projet_bdda_chaabnia_fekihhassen_benmansour_nadarajah/Record.java
@@ -1,105 +1,114 @@
 package eclipse_projet_bdda_chaabnia_fekihhassen_benmansour_nadarajah;
-
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
 
-//Ajouter des exceptions  -> try catch()
 public class Record {
-	private TableInfo tabInfo;
-	private ArrayList<String> recvalues;
+    private TableInfo tabInfo;
+    private ArrayList<String> recvalues;
 
-	public Record(TableInfo tabInfo) {
-		this.tabInfo = tabInfo;
-		this.recvalues = new ArrayList<String>();
-	}
+    public Record(TableInfo tabInfo) {
+        this.tabInfo = tabInfo;
+        this.recvalues = new ArrayList<>();
+    }
 
-	// FAIRE UN DEUXIEME CONSTRUCTEUR
+    // Ajoutez un deuxième constructeur si nécessaire
 
-	private int writeToBuffer(ByteBuffer buff, int pos) {
-		// Position the cursor at the specified position
-		buff.position(pos);
+    public int writeToBuffer(ByteBuffer buff, int pos) {
+        // Positionne le curseur au début de l'écriture
+        buff.position(pos);
 
-		for (int i = 0; i < tabInfo.getColInfoList().size(); i++) {
-			if (tabInfo.getColInfoList().get(i).getName().equals("INT")) {
-				int intValue = Integer.parseInt(recvalues.get(i));
-				buff.putInt(intValue);
-			}
-			if (tabInfo.getColInfoList().get(i).equals("FLOAT")) {
-				float floatValue = Float.parseFloat(recvalues.get(i));
-				buff.putFloat(floatValue);
-			}
-			if (tabInfo.getColInfoList().get(i).equals("STRING")) {
-				String stringValue = recvalues.get(i);
-				for (char c : stringValue.toCharArray()) {
-					buff.putChar(c);
-				}
-			}
-			if (tabInfo.getColInfoList().get(i).equals("VARSTRING")) {
-				String stringValue = recvalues.get(i);
-				buff.putInt(stringValue.length());
-				for (char c : stringValue.toCharArray()) {
-					buff.putChar(c);
-				}
-			}
-		}
+        for (int i = 0; i < tabInfo.getColInfoList().size(); i++) {
+            String colType = tabInfo.getColInfoList().get(i).getType();
+            String value = recvalues.get(i);
 
-		// Return the total size (number of bytes) written to the buffer
-		return buff.position() - pos;
-	}
+            switch (colType) {
+                case "INT":
+                    int intValue = Integer.parseInt(value);
+                    buff.putInt(intValue);
+                    break;
+                case "FLOAT":
+                    float floatValue = Float.parseFloat(value);
+                    buff.putFloat(floatValue);
+                    break;
+                case "STRING":
+                    for (char c : value.toCharArray()) {
+                        buff.putChar(c);
+                    }
+                    buff.putChar('\0'); 
+                    break;
+                case "VARSTRING":
+                    int length = value.length();
+                    buff.putInt(length);
+                    for (char c : value.toCharArray()) {
+                        buff.putChar(c);
+                    }
+                    break;
+                default:
+                    throw new IllegalArgumentException("type de colonne inconnu : " + colType);
+            }
+        }
 
-	private int readFromBuffer(ByteBuffer buff, int pos) {
-		// Position the cursor at the specified position
-		buff.position(pos);
+        return buff.position() - pos;
+    }
 
-		// Clear the existing values in the recvalues list
-		recvalues.clear();
+    public int readFromBuffer(ByteBuffer buff, int pos) {
+        // Positionne le curseur au début de la lecture
+        buff.position(pos);
+    
+        // Efface les valeurs existantes dans la liste recvalues
+        recvalues.clear();
+    
+        for (int i = 0; i < tabInfo.getColInfoList().size(); i++) {
+            String colType = tabInfo.getColInfoList().get(i).getType();
+    
+            switch (colType) {
+                case "INT":
+                    int intValue = buff.getInt();
+                    recvalues.add(Integer.toString(intValue));
+                    break;
+                case "FLOAT":
+                    float floatValue = buff.getFloat();
+                    recvalues.add(Float.toString(floatValue));
+                    break;
+                case "STRING":
+                    StringBuilder stringValue = new StringBuilder();
+                    char c;
+                    while ((c = buff.getChar()) != '\0') {
+                        stringValue.append(c);
+                    }
+                    recvalues.add(stringValue.toString());
+                    break;
+                case "VARSTRING":
+                    int length = buff.getInt();
+                    StringBuilder varStringValue = new StringBuilder();
+                    for (int j = 0; j < length; j++) {
+                        char varChar = buff.getChar(); 
+                        varStringValue.append(varChar);
+                    }
+                    recvalues.add(varStringValue.toString());
+                    break;
+                default:
+                    throw new IllegalArgumentException("Type de colonne inconnu : " + colType);
+            }
+        }
+    
+        return buff.position() - pos;
+    }
+    
 
-		for (int i = 0; i < tabInfo.getColInfoList().size(); i++) {
-			if (tabInfo.getColInfoList().get(i).getName().equals("INT")) {
-				int intValue = buff.getInt();
-				recvalues.add(Integer.toString(intValue));
-			} else if (tabInfo.getColInfoList().get(i).getName().equals("FLOAT")) {
-				float floatValue = buff.getFloat();
-				recvalues.add(Float.toString(floatValue));
-			} else if (tabInfo.getColInfoList().get(i).getName().equals("STRING")) {
-				StringBuilder stringValue = new StringBuilder();
-				while (buff.hasRemaining()) {
-					char c = buff.getChar();
-					if (c == '\0') {
-						break;
-					}
-					stringValue.append(c);
-				}
-				recvalues.add(stringValue.toString());
-			} else if (tabInfo.getColInfoList().get(i).getName().equals("VARSTRING")) {
-				int length = buff.getInt();
-				StringBuilder stringValue = new StringBuilder();
-				for (int j = 0; j < length; j++) {
-					char c = buff.getChar();
-					stringValue.append(c);
-				}
-				recvalues.add(stringValue.toString());
-			}
+    public TableInfo getTabInfo() {
+        return tabInfo;
+    }
 
-			// Return the total size (number of bytes) read from the buffer
-			return buff.position() - pos;
-		}
-	}
+    public void setTabInfo(TableInfo tabInfo) {
+        this.tabInfo = tabInfo;
+    }
 
-	public TableInfo getTabInfo() {
-		return tabInfo;
-	}
+    public ArrayList<String> getRecvalues() {
+        return recvalues;
+    }
 
-	public void setTabInfo(TableInfo tabInfo) {
-		this.tabInfo = tabInfo;
-	}
-
-	public ArrayList<String> getRecvalues() {
-		return recvalues;
-	}
-
-	public void setRecvalues(ArrayList<String> recvalues) {
-		this.recvalues = recvalues;
-	}
-
+    public void setRecvalues(ArrayList<String> recvalues) {
+        this.recvalues = recvalues;
+    }
 }
