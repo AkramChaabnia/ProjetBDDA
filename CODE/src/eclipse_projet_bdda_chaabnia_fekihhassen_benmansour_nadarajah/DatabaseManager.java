594ff1a62db31256592e52ea17f51cc01db89f3d
snaazy <yassinefekih78@outlook.fr>
2023-12-06 22:08:48 +0100

    test

--- CODE/src/eclipse_projet_bdda_chaabnia_fekihhassen_benmansour_nadarajah/DatabaseManager.java
+++ CODE/src/eclipse_projet_bdda_chaabnia_fekihhassen_benmansour_nadarajah/DatabaseManager.java
@@ -1,5 +1,51 @@
-package eclipse_projet_bdda_chaabnia_fekihhassen_benmansour_nadarajah;
+/* package eclipse_projet_bdda_chaabnia_fekihhassen_benmansour_nadarajah;
+
 
 public class DatabaseManager {
-    
+    private static DatabaseManager instance = null;
+
+    private DatabaseManager() {
+        // Constructeur privé pour assurer une seule instance
+    }
+
+    public static DatabaseManager getInstance() {
+        if (instance == null) {
+            instance = new DatabaseManager();
+        }
+        return instance;
+    }
+
+    public void init() {
+        // Appel à la méthode init du DataBaseInfo
+        DataBaseInfo.getInstance().Init();
+
+        // Appel à la méthode init du BufferManager
+        BufferManager.getInstance().init();
+
+        // Si une méthode init du DiskManager existe, ajoutez cet appel ici
+    }
+
+    public void finish() {
+        // Si une méthode finish du DiskManager existe, ajoutez cet appel ici
+
+        // Appel à la méthode FlushAll (ou Finish) du BufferManager
+        BufferManager.getInstance().flushAll();
+
+        // Appel à la méthode finish du DataBaseInfo
+        DataBaseInfo.getInstance().finish();
+    }
+
+    public void processCommand(String command) {
+        // Vous allez implémenter le traitement des commandes ici
+        // Utilisez un switch/case ou des if/else pour gérer différentes commandes
+        // Par exemple :
+        // if (command.startsWith("SELECT")) {
+        //     // Traitement de la commande SELECT
+        // } else if (command.startsWith("INSERT")) {
+        //     // Traitement de la commande INSERT
+        // } else if (...) {
+        //     // Autres commandes
+        // }
+    }
 }
+ */
--- CODE/src/eclipse_projet_bdda_chaabnia_fekihhassen_benmansour_nadarajah/Record.java
+++ CODE/src/eclipse_projet_bdda_chaabnia_fekihhassen_benmansour_nadarajah/Record.java
@@ -4,112 +4,146 @@ import java.nio.ByteBuffer;
 import java.util.ArrayList;
 
 public class Record {
-    private TableInfo tabInfo;
-    private ArrayList<String> recvalues;
+	private TableInfo tabInfo;
+	private ArrayList<String> recvalues;
+	private int size;
 
-    public Record(TableInfo tabInfo) {
-        this.tabInfo = tabInfo;
-        this.recvalues = new ArrayList<>();
-    }
+	public Record(TableInfo tabInfo) {
+		this.tabInfo = tabInfo;
+		this.recvalues = new ArrayList<>();
+		this.size = calculateSize();
+	}
 
-    // Ajoutez un deuxième constructeur si nécessaire
+	// Méthode privée pour calculer la taille de l'enregistrement
+	private int calculateSize() {
+		int recordSize = 0;
 
-    public int writeToBuffer(ByteBuffer buff, int pos) {
-        // Positionne le curseur au début de l'écriture
-        buff.position(pos);
+		for (int i = 0; i < tabInfo.getColInfoList().size(); i++) {
+			String colType = tabInfo.getColInfoList().get(i).getType();
 
-        for (int i = 0; i < tabInfo.getColInfoList().size(); i++) {
-            String colType = tabInfo.getColInfoList().get(i).getType();
-            String value = recvalues.get(i);
+			switch (colType) {
+				case "INT":
+					recordSize += Integer.BYTES;
+					break;
+				case "FLOAT":
+					recordSize += Float.BYTES;
+					break;
+				case "STRING":
+					recordSize += recvalues.get(i).length() * Character.BYTES + Character.BYTES; // Inclure le caractère
+																									// nul
+					break;
+				case "VARSTRING":
+					recordSize += Integer.BYTES + recvalues.get(i).length() * Character.BYTES;
+					break;
+				default:
+					throw new IllegalArgumentException("Type de colonne inconnu : " + colType);
+			}
+		}
 
-            switch (colType) {
-                case "INT":
-                    int intValue = Integer.parseInt(value);
-                    buff.putInt(intValue);
-                    break;
-                case "FLOAT":
-                    float floatValue = Float.parseFloat(value);
-                    buff.putFloat(floatValue);
-                    break;
-                case "STRING":
-                    for (char c : value.toCharArray()) {
-                        buff.putChar(c);
-                    }
-                    buff.putChar('\0'); 
-                    break;
-                case "VARSTRING":
-                    int length = value.length();
-                    buff.putInt(length);
-                    for (char c : value.toCharArray()) {
-                        buff.putChar(c);
-                    }
-                    break;
-                default:
-                    throw new IllegalArgumentException("type de colonne inconnu : " + colType);
-            }
-        }
+		return recordSize;
+	}
 
-        return buff.position() - pos;
-    }
+	public int getSize() {
+		return size;
+	}
 
-    public int readFromBuffer(ByteBuffer buff, int pos) {
-        // Positionne le curseur au début de la lecture
-        buff.position(pos);
-    
-        // Efface les valeurs existantes dans la liste recvalues
-        recvalues.clear();
-    
-        for (int i = 0; i < tabInfo.getColInfoList().size(); i++) {
-            String colType = tabInfo.getColInfoList().get(i).getType();
-    
-            switch (colType) {
-                case "INT":
-                    int intValue = buff.getInt();
-                    recvalues.add(Integer.toString(intValue));
-                    break;
-                case "FLOAT":
-                    float floatValue = buff.getFloat();
-                    recvalues.add(Float.toString(floatValue));
-                    break;
-                case "STRING":
-                    StringBuilder stringValue = new StringBuilder();
-                    char c;
-                    while ((c = buff.getChar()) != '\0') {
-                        stringValue.append(c);
-                    }
-                    recvalues.add(stringValue.toString());
-                    break;
-                case "VARSTRING":
-                    int length = buff.getInt();
-                    StringBuilder varStringValue = new StringBuilder();
-                    for (int j = 0; j < length; j++) {
-                        char varChar = buff.getChar(); 
-                        varStringValue.append(varChar);
-                    }
-                    recvalues.add(varStringValue.toString());
-                    break;
-                default:
-                    throw new IllegalArgumentException("Type de colonne inconnu : " + colType);
-            }
-        }
-    
-        return buff.position() - pos;
-    }
-    
+	public int writeToBuffer(ByteBuffer buff, int pos) {
+		// Positionne le curseur au début de l'écriture
+		buff.position(pos);
 
-    public TableInfo getTabInfo() {
-        return tabInfo;
-    }
+		for (int i = 0; i < tabInfo.getColInfoList().size(); i++) {
+			String colType = tabInfo.getColInfoList().get(i).getType();
+			String value = recvalues.get(i);
 
-    public void setTabInfo(TableInfo tabInfo) {
-        this.tabInfo = tabInfo;
-    }
+			switch (colType) {
+				case "INT":
+					int intValue = Integer.parseInt(value);
+					buff.putInt(intValue);
+					break;
+				case "FLOAT":
+					float floatValue = Float.parseFloat(value);
+					buff.putFloat(floatValue);
+					break;
+				case "STRING":
+					for (char c : value.toCharArray()) {
+						buff.putChar(c);
+					}
+					buff.putChar('\0');
+					break;
+				case "VARSTRING":
+					int length = value.length();
+					buff.putInt(length);
+					for (char c : value.toCharArray()) {
+						buff.putChar(c);
+					}
+					break;
+				default:
+					throw new IllegalArgumentException("type de colonne inconnu : " + colType);
+			}
+		}
 
-    public ArrayList<String> getRecvalues() {
-        return recvalues;
-    }
+		return buff.position() - pos;
+	}
 
-    public void setRecvalues(ArrayList<String> recvalues) {
-        this.recvalues = recvalues;
-    }
+	public int readFromBuffer(ByteBuffer buff, int pos) {
+		// Positionne le curseur au début de la lecture
+		buff.position(pos);
+
+		// Efface les valeurs existantes dans la liste recvalues
+		recvalues.clear();
+
+		for (int i = 0; i < tabInfo.getColInfoList().size(); i++) {
+			String colType = tabInfo.getColInfoList().get(i).getType();
+
+			switch (colType) {
+				case "INT":
+					int intValue = buff.getInt();
+					recvalues.add(Integer.toString(intValue));
+					break;
+				case "FLOAT":
+					float floatValue = buff.getFloat();
+					recvalues.add(Float.toString(floatValue));
+					break;
+				case "STRING":
+					StringBuilder stringValue = new StringBuilder();
+					char c;
+					while ((c = buff.getChar()) != '\0') {
+						stringValue.append(c);
+					}
+					recvalues.add(stringValue.toString());
+					break;
+				case "VARSTRING":
+					int length = buff.getInt();
+					StringBuilder varStringValue = new StringBuilder();
+					for (int j = 0; j < length; j++) {
+						char varChar = buff.getChar();
+						varStringValue.append(varChar);
+					}
+					recvalues.add(varStringValue.toString());
+					break;
+				default:
+					throw new IllegalArgumentException("Type de colonne inconnu : " + colType);
+			}
+		}
+
+		return buff.position() - pos;
+	}
+
+	public TableInfo getTabInfo() {
+		return tabInfo;
+	}
+
+	public void setTabInfo(TableInfo tabInfo) {
+		this.tabInfo = tabInfo;
+	}
+
+	public ArrayList<String> getRecvalues() {
+		return recvalues;
+	}
+
+	public void setRecvalues(ArrayList<String> recvalues) {
+		this.recvalues = recvalues;
+		this.size = calculateSize();
+	}
+
 }
