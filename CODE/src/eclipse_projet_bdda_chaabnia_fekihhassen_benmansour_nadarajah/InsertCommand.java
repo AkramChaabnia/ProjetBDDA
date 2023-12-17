adcc220aa41068dd0a82047b6b097f3ed205dc12
akramch <akram.chaabnia25@gmail.com>
2023-12-17 15:13:21 +0100

    TP6: InsertCommand fileManager.insertIntoTable(tableInfo, values);

---
+++ CODE/src/eclipse_projet_bdda_chaabnia_fekihhassen_benmansour_nadarajah/InsertCommand.java
@@ -0,0 +1,28 @@
+package eclipse_projet_bdda_chaabnia_fekihhassen_benmansour_nadarajah;
+
+import java.util.ArrayList;
+
+public class InsertCommand {
+    private String nom_relation;
+    private ArrayList<String> values;
+    FileManager fileManager = FileManager.getInstance();
+
+    public InsertCommand(String command) {
+        String[] cmd = command.split(" ");
+        this.nom_relation = cmd[2];
+        this.values = new ArrayList<>();
+        String[] vals = cmd[4].substring(1, cmd[4].length() - 1).split(",");
+        for (String val : vals) {
+            values.add(val);
+        }
+    }
+
+    public void execute() {
+        TableInfo tableInfo = DataBaseInfo.getInstance().getTableInfo(nom_relation);
+        if (tableInfo != null) {
+            // fileManager.insertIntoTable(tableInfo, values);
+        } else {
+            System.out.println("Table " + nom_relation + " does not exist.");
+        }
+    }
+}
