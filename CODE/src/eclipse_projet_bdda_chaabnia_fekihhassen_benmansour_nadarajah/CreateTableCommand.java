5f8fa2e6df56cc9125be910ec6b306df14283846
snaazy <yassinefekih78@outlook.fr>
2023-12-18 22:35:47 +0100

    CreateTableCommand

--- CODE/src/eclipse_projet_bdda_chaabnia_fekihhassen_benmansour_nadarajah/CreateTableCommand.java
+++ CODE/src/eclipse_projet_bdda_chaabnia_fekihhassen_benmansour_nadarajah/CreateTableCommand.java
@@ -10,27 +10,27 @@ public class CreateTableCommand {
 
   public CreateTableCommand(String command) {
     try {
-        String[] cmd = command.split(" ");
-        if (cmd.length < 4) {
-            throw new IllegalArgumentException("Commande CREATE TABLE incomplète");
+      String[] cmd = command.split(" ");
+      if (cmd.length < 4) {
+        throw new IllegalArgumentException("Commande CREATE TABLE incomplète");
+      }
+      this.nom_relation = cmd[2];
+      this.colInfoList = new ArrayList<>();
+      String[] columns = cmd[3].substring(1, cmd[3].length() - 1).split(",");
+      for (String column : columns) {
+        String[] colInfo = column.split(":");
+        if (colInfo.length != 2) {
+          throw new IllegalArgumentException("Format incorrect pour la définition des colonnes");
         }
-        this.nom_relation = cmd[2];
-        this.colInfoList = new ArrayList<>();
-        String[] columns = cmd[3].substring(1, cmd[3].length() - 1).split(",");
-        for (String column : columns) {
-            String[] colInfo = column.split(":");
-            if (colInfo.length != 2) {
-                throw new IllegalArgumentException("Format incorrect pour la définition des colonnes");
-            }
-            String colName = colInfo[0];
-            String colType = colInfo[1];
-            ColInfo col = new ColInfo(colName, colType);
-            colInfoList.add(col);
-        }
+        String colName = colInfo[0];
+        String colType = colInfo[1];
+        ColInfo col = new ColInfo(colName, colType);
+        colInfoList.add(col);
+      }
     } catch (Exception e) {
-        System.out.println("Erreur lors du parsing de la commande : " + e.getMessage());
+      System.out.println("Erreur lors du parsing de la commande : " + e.getMessage());
     }
-}
+  }
 
   public void execute() {
     DataBaseInfo databaseInfo = DataBaseInfo.getInstance();
@@ -46,4 +46,5 @@ public class CreateTableCommand {
       e.printStackTrace();
     }
   }
+
 }
