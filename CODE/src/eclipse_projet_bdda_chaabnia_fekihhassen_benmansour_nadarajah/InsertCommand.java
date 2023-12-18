1f7febf22821c91ade7c3138b14b66f3723819d7
snaazy <yassinefekih78@outlook.fr>
2023-12-18 22:56:31 +0100

    insert

--- CODE/src/eclipse_projet_bdda_chaabnia_fekihhassen_benmansour_nadarajah/InsertCommand.java
+++ CODE/src/eclipse_projet_bdda_chaabnia_fekihhassen_benmansour_nadarajah/InsertCommand.java
@@ -3,26 +3,46 @@ package eclipse_projet_bdda_chaabnia_fekihhassen_benmansour_nadarajah;
 import java.util.ArrayList;
 
 public class InsertCommand {
-    private String nom_relation;
+    private String nomRelation;
     private ArrayList<String> values;
     FileManager fileManager = FileManager.getInstance();
 
     public InsertCommand(String command) {
-        String[] cmd = command.split(" ");
-        this.nom_relation = cmd[2];
+        parseCommand(command);
+    }
+
+    private void parseCommand(String command) {
+        String[] parts = command.split("VALUES");
+        if (parts.length != 2) {
+            throw new IllegalArgumentException("Format incorrect pour la commande INSERT");
+        }
+
+        this.nomRelation = parts[0].split("\\s+")[2].trim();
+        String valuePart = parts[1].trim();
+        valuePart = valuePart.substring(1, valuePart.length() - 1);
         this.values = new ArrayList<>();
-        String[] vals = cmd[4].substring(1, cmd[4].length() - 1).split(",");
-        for (String val : vals) {
-            values.add(val);
+        for (String val : valuePart.split(",")) {
+            values.add(val.trim());
         }
     }
 
+
     public void execute() {
-        TableInfo tableInfo = DataBaseInfo.getInstance().getTableInfo(nom_relation);
-        if (tableInfo != null) {
-            fileManager.insertIntoTable(tableInfo, values);
-        } else {
-            System.out.println("Table " + nom_relation + " does not exist.");
+        try {
+            TableInfo tableInfo = DataBaseInfo.getInstance().getTableInfo(nomRelation);
+            if (tableInfo == null) {
+                throw new IllegalArgumentException("Table non trouvée : " + nomRelation);
+            }
+
+
+            Record record = new Record(tableInfo);
+            for (String value : values) {
+                record.addValue(value);
+            }
+
+            // inserer record ici?
+        } catch (Exception e) {
+            System.out.println("Erreur lors de l'exécution de la commande INSERT : " + e.getMessage());
         }
     }
 }
