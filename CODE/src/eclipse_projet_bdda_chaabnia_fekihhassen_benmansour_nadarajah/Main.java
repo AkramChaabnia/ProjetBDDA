504c0226ea6e04ee416a67cd1e8f1fa1b3631caf
akramch <akram.chaabnia25@gmail.com>
2023-12-20 18:20:27 +0100

    ajout de try catch dans le main

--- CODE/src/eclipse_projet_bdda_chaabnia_fekihhassen_benmansour_nadarajah/Main.java
+++ CODE/src/eclipse_projet_bdda_chaabnia_fekihhassen_benmansour_nadarajah/Main.java
@@ -7,62 +7,65 @@ import java.util.Iterator;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException, PageNotFoundException {
+	public static void main(String[] args) {
 
-		DBParams.DBPath = "BD//";
-		// DBParams.DBPath = args[0];
+		try {
+			DBParams.DBPath = "BD\\"; // DBParams.DBPath = args[0];
 
-		DBParams.SGBDPageSize = 4096;
-		DBParams.DMFileCount = 4;
-		DBParams.FrameCount = 2;
+			DBParams.SGBDPageSize = 4096;
+			DBParams.DMFileCount = 4;
+			DBParams.FrameCount = 2;
 
-		// Call the init method
-		DataBaseManager dbManager = DataBaseManager.getInstance();
-		dbManager.init();
+			// Call the init method
+			DataBaseManager dbManager = DataBaseManager.getInstance();
+			dbManager.init();
 
-		String command;
-		String upperCaseCommand;
-		String[] commands = { "HELP", "EXIT", "CREATE RELATION <Relation> (c1:type,c2:type,...)", "RESETDB",
-				"INSERT INTO <Relation> VALUES (val1,val2)",
-				"SELECT * FROM <RELATION>", };
+			String command;
+			String upperCaseCommand;
+			String[] commands = { "HELP", "EXIT", "CREATE RELATION <Relation> (c1:type,c2:type,...)", "RESETDB",
+					"INSERT INTO <Relation> VALUES (val1,val2)",
+					"SELECT * FROM <RELATION>", };
 
-		List<String> testCommands = Arrays.asList(
-				"RESETDB",
-				"CREATE TABLE R (C1:INT,C2:VARSTRING(3),C3:INT)",
-				"INSERT INTO R VALUES (1,aab,2)",
-				"INSERT INTO R VALUES (2,ab,2)",
-				"INSERT INTO R VALUES (1,agh,1)",
-				"SELECT * FROM R",
-				"SELECT * FROM R WHERE C1=1",
-				"SELECT * FROM R WHERE C3=1",
-				"SELECT * FROM R WHERE C1=1 AND C3=2",
-				"SELECT * FROM R WHERE C1<2",
-				"EXIT");
+			List<String> testCommands = Arrays.asList(
+					"RESETDB",
+					"CREATE TABLE R (C1:INT,C2:VARSTRING(3),C3:INT)",
+					"INSERT INTO R VALUES (1,aab,2)",
+					"INSERT INTO R VALUES (2,ab,2)",
+					"INSERT INTO R VALUES (1,agh,1)",
+					"SELECT * FROM R",
+					"SELECT * FROM R WHERE C1=1",
+					"SELECT * FROM R WHERE C3=1",
+					"SELECT * FROM R WHERE C1=1 AND C3=2",
+					"SELECT * FROM R WHERE C1<2",
+					"EXIT");
 
-		Iterator<String> testCommandIterator = testCommands.iterator();
+			Iterator<String> testCommandIterator = testCommands.iterator();
 
-		do {
-			if (testCommandIterator.hasNext()) {
-				command = testCommandIterator.next();
-			} else {
-				break;
-			}
-
-			System.out.println("Executing command: " + command);
-
-			upperCaseCommand = command.toUpperCase();
-			switch (upperCaseCommand) {
-				case "HELP":
-					System.out.println("Commandes disponibles : " + Arrays.toString(commands));
+			do {
+				if (testCommandIterator.hasNext()) {
+					command = testCommandIterator.next();
+				} else {
 					break;
-				case "EXIT":
-					System.out.println("Au revoir!\n");
-					// dbManager.finish();
-					break;
-				default:
-					dbManager.processCommand(command);
-					break;
-			}
-		} while (!upperCaseCommand.equals("EXIT"));
+				}
+
+				System.out.println("Executing command: " + command);
+
+				upperCaseCommand = command.toUpperCase();
+				switch (upperCaseCommand) {
+					case "HELP":
+						System.out.println("Commandes disponibles : " + Arrays.toString(commands));
+						break;
+					case "EXIT":
+						System.out.println("Au revoir!\n");
+						// dbManager.finish();
+						break;
+					default:
+						dbManager.processCommand(command);
+						break;
+				}
+			} while (!upperCaseCommand.equals("EXIT"));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
