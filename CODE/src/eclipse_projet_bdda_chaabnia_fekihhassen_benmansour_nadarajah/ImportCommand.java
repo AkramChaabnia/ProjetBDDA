07495f23155d6a565eab3337ed397f44ada51bcb
snaazy <yassinefekih78@outlook.fr>
2023-12-20 15:03:45 +0100

    Constructeur/parsing

--- CODE/src/eclipse_projet_bdda_chaabnia_fekihhassen_benmansour_nadarajah/ImportCommand.java
+++ CODE/src/eclipse_projet_bdda_chaabnia_fekihhassen_benmansour_nadarajah/ImportCommand.java
@@ -1,5 +1,26 @@
 package eclipse_projet_bdda_chaabnia_fekihhassen_benmansour_nadarajah;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+
 public class ImportCommand {
-    
+  private String relationName;
+  private String csvFileName;
+
+  public ImportCommand(String command) {
+    parseCommand(command);
+  }
+
+  private void parseCommand(String command) {
+    String[] commandParts = command.split(" ");
+    if (commandParts.length != 4 || !commandParts[0].equalsIgnoreCase("IMPORT") || !commandParts[2].equalsIgnoreCase("INTO")) {
+      throw new IllegalArgumentException("Commande d'importation incorrecte.");
+    }
+
+    this.relationName = commandParts[1];
+    this.csvFileName = commandParts[3];
+  }
+
 }
